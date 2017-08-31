108c758169bd77d1473a224510cbcee115a74b24
Eric Cao <sheepcao1986@163.com>
2017-08-31 23:13:14 +0800

    Apply MVVM and listed basic file structure

---
+++ ECMoments/Moments/View/ECMomentsViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ECMoments
+//
+//  Created by EricCao on 8/31/17.
+//  Copyright © 2017 EricCao. All rights reserved.
+//
+
+#import "ECMomentsViewController.h"
+
+@interface ECMomentsViewController ()
+
+@end
+
+@implementation ECMomentsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
